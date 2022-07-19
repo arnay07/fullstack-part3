1db7958eccb46b48095b6fbaca25a8fe971bf045
arnaud kaderi <arnay07@gmail.com>
2022-07-19 10:19:04 +0200

    deployment to the backend

---
+++ index.js
@@ -0,0 +1,110 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
+
+//FIXME: make this middleware work
+morgan.token('payload', (req) => {
+  return JSON.stringify(req.body);
+});
+
+app.use(morgan(':method :url :status :response-time ms :payload'));
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+  const dateString = date.toDateString();
+  res.send(
+    `
+        <p>Phonebook has info for ${persons.length} people</p>
+        <p>${dateString} ${time}</p>
+        `
+  );
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: 'Name or number missing' });
+  }
+
+  const existingPerson = persons.find((person) => person.name === body.name);
+
+  if (existingPerson) {
+    return res.status(400).json({ error: 'Name must be unique' });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 100000),
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
